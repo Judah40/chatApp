989861137473834cfae9ba1a7e593e3319299284
Judah Alvin Dore <judahdore@gmail.com>
2025-11-21 02:32:52 +0000

    second commit

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = function (api) {
+  api.cache(true);
+  const plugins = [
+    // "react-native-paper/babel",
+    "react-native-reanimated/plugin",
+  ];
+
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins,
+  };
+};
